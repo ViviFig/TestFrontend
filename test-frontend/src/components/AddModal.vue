101dd22a32a34bd11525db2afb1194814273e023
Viviana Figus <viviana.figus@gmail.com>
2023-02-13 19:18:59 +0100

    Add POST request

--- test-frontend/src/components/AddModal.vue
+++ test-frontend/src/components/AddModal.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" @click="ToggleModal()">
+    <div class="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
+        @click="ToggleModal()">
         <div class="modal-dialog " @click.stop="">
             <div class="modal-content modal-large px-5 py-4">
                 <div class="modal-body">
@@ -19,28 +20,29 @@
                             <input type="date" class="form-control" id="recipient-name" value="2023-01-30">
                         </div>
 
-                        
+
                         <div class="mb-3">
                             <label for="recipient-name" class="col-form-label">Genre</label>
                             <div class="form-check">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                            <label class="form-check-label" for="flexRadioDefault1">
-                                Default radio
-                            </label>
+                                <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                    id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    Default radio
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                    id="flexRadioDefault2" checked>
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    Default checked radio
+                                </label>
+                            </div>
                         </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"
-                                checked>
-                            <label class="form-check-label" for="flexRadioDefault2">
-                                Default checked radio
-                            </label>
-                        </div>
-                        </div>
-                      
+
                     </form>
                 </div>
-                <div class="modal-footer justify-content-center">             
-                    <button type="button" class="btn btn-primary btn-sm">Save</button>
+                <div class="modal-footer justify-content-center">
+                    <button type="button" class="btn btn-primary btn-sm" @click="postData">Save</button>
                 </div>
             </div>
         </div>
@@ -50,8 +52,46 @@
 
 <script>
 export default {
+
+    data() {
+        return {
+            newGame: {
+                label: "TestGame",
+                description: "Testdescription",
+                date: "2023-02-13T18:00:43.791Z",
+                color: "Black"
+            }
+        }
+
+    },
     name: "add-modal",
 
-    props: ['ToggleModal']
+    props: ['ToggleModal'],
+
+    methods: {
+        postData() {
+            fetch('https://frontend-test-api.anothereality.io/data/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    label: this.newGame.label,
+                    description: this.newGame.description,
+                    date: this.newGame.date,
+                    color: this.newGame.color
+
+                })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+    }
+
 };
 </script>
